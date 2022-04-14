e263e4e195b836dd8affa283192ecb580b59b299
Reaper <ahoy@barelyhuman.dev>
2022-04-14 14:52:19 +0530

    remove test methods from asyncPipe

--- src/asyncPipe.ts
+++ src/asyncPipe.ts
@@ -1,24 +1,15 @@
 type Operation = (data?: any) => Promise<any>;
 
 export default async function asyncPipe(...args: Operation[]) {
-  try {
-    const mappedResults = [];
-    // create a sequential chain of operations
-    await args.reduce((chain, operation: Operation) => {
-      return chain
-        .then((prevData?: any) => {
-          // take in the previous data and pass it down
-          return operation(prevData);
-        })
-        .then((opData) => {
-          // push data into overall sequential result and
-          // return to the next possible operation
-          mappedResults.push(opData);
-          return opData;
-        });
-    }, Promise.resolve());
-    return mappedResults;
-  } catch (err) {
-    return false;
-  }
+  return await args.reduce((chain, operation: Operation) => {
+    return chain
+      .then((prevData?: any) => {
+        // take in the previous data and pass it down
+        return operation(prevData);
+      })
+      .then((opData) => {
+        // return to the next possible operation
+        return opData;
+      });
+  }, Promise.resolve());
 }
