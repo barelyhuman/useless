deed6f33653fd5fbec4d6bb0aa32cf2a6502fba8
Reaper <ahoy@barelyhuman.dev>
2022-04-14 14:59:39 +0530

    add entry point

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { default as asyncPipe } from "./asyncPipe";
+export { default as chunk } from "./chunk";
+export { default as createExecTime } from "./createExecTime";
+export { default as timestringParser } from "./timestringParser";
+export { default as toTitle } from "./toTitle";
+export { default as toUTCStartOfMonth } from "./toUTCStartOfMonth";
+export { default as toUTCEndOfMonth } from "./toUTCEndOfMonth";
