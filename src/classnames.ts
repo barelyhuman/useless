182efe9bdb23d5233e5e758748dfaccfce28f560
Reaper <ahoy@barelyhuman.dev>
2022-04-14 18:48:42 +0530

    add classnames + example

--- src/classnames.ts
+++ src/classnames.ts
@@ -1,3 +1,13 @@
+/**
+ * Convert a given string of classname , or conditional objects, convert them
+ * to a single classlist string
+ *
+ *  #### Example
+ * ```js
+ *  import classnames from "@barelyhuman/useless/classnames"
+ *  classnames("input",{active: isActive}) //=> "input" || "input active" (if `isActive` is true)
+ * ```
+ */
 export default function classnames(...args: (string | object)[]) {
   const classlist = [];
   for (let index = 0; index < args.length; index++) {
