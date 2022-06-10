5faf8dff1209f901eecf7c51476d1df462b6e3b9
Reaper <ahoy@barelyhuman.dev>
2022-06-10 11:32:46 +0530

    refactor: asyncSerial , perf changes from conch

--- src/asyncSerial.ts
+++ src/asyncSerial.ts
@@ -44,47 +44,34 @@ export interface Options {
 	limit?: number
 }
 
-export default function asyncSerial<T extends any>(
-	iterable: Iterable<T>,
+export default function asyncSerial<T>(
+	iterable: Array<T>,
 	mapFunc: MapFunction<T>,
 	options: Options = {limit: Infinity},
 ) {
-	return new Promise((resolve, reject) => {
-		let result = []
-		const limit = options.limit
-		const iterator = [...iterable]
+	let resolved = []
+	let limit = options.limit
+	let offset = 0
+	let chain = Promise.resolve()
 
-		// Check for limit to be a valid number and not less that 1
-		if (isNaN(limit) || limit < 1) {
-			throw new Error(
-				'Invalid number returned for limit, make sure its a number and is greater than 1',
-			)
-		}
+	if (!(iterable instanceof Array))
+		throw new Error('[conch] the input needs to be an array of items')
 
-		// store the total number of chunks to be created for the provided limit
-		let totalChunks = 1
+	if (isNaN(options.limit))
+		throw new Error('[conch] limit needs to be a number')
 
-		if (limit >= 1 && limit !== Infinity) {
-			totalChunks = Math.ceil(iterator.length / limit)
-		}
+	if (options.limit === Infinity || options.limit === 0) limit = iterable.length
 
-		// Create an array out of the total batches
-		;[...Array(totalChunks).keys()]
+	while (offset < iterable.length) {
+		const batch = iterable.slice(offset * limit, (offset + 1) * limit)
+		if (batch.length === 0) break
+		offset = offset + 1
+		chain = chain
+			.then(() => Promise.all(batch.map(mapFunc)))
+			.then(d => {
+				resolved = resolved.concat(d)
+			})
+	}
 
-			// go through each item while slicing it into batches and processing a single batch
-			// then create a promise chain resolving one batch after the other
-			.reduce((acc, _, index) => {
-				const batch = iterator.slice(index * limit, (index + 1) * limit)
-
-				return acc
-					.then(() => Promise.all(batch.map(mapFunc)))
-					.then(data => {
-						result = result.concat(data)
-					})
-					.catch(reject)
-			}, Promise.resolve())
-
-			// return the completed result
-			.then(_ => resolve(result))
-	})
+	return chain.then(() => resolved)
 }
